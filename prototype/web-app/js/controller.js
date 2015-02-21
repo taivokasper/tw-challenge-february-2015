894bd5c4af2f4aa17febd9ea3a226118e7ee34ea
andrestaht <andres.taht@gmail.com>
2015-02-21 15:30:40 +0200

    Tegin midagi

--- prototype/web-app/js/controller.js
+++ prototype/web-app/js/controller.js
@@ -5,5 +5,10 @@ app.controller('IndexCtrl', function ($scope) {
 });
 
 app.controller('PayCtrl', function ($scope) {
-
+	$scope.data = {
+		'receiver': '',
+		'account_number': '',
+		'bill_number': '',
+		'reference_number': ''
+	};
 });
